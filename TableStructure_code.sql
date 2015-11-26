e82b8661998f08614ac0774028c81f3388f25a68
SergeyValevskyi <w0934014151@gmail.com>
2015-11-27 01:41:28 +0200

    Structur DB + formatted text

--- TableStructure_code.sql
+++ TableStructure_code.sql
@@ -1,82 +1,43 @@
-CREATE SEQUENCE object_sequence START WITH 1 INCREMENT BY 1;
--- CREATE SEQUENCE attribute_sequence START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE object_id START WITH 1 INCREMENT BY 1;
+--CREATE SEQUENCE attrtype_id START WITH 1 INCREMENT BY 1;
+--Чтобы не вытаскивать каждый раз ид я пока оставил видимыми идишники.
 
--- <OBJTYPE> ------------------------------------------------------------------
+
 -- Add classes: User, Task, Subtask
-INSERT INTO OBJTYPE (OBJECT_TYPE_ID, PARENT_ID, CODE, NAME, DESCRIPTION) 
+INSERT INTO objtype (object_type_id, parent_id, code, name, description)
 VALUES (1, NULL, 'Task', 'Задача', NULL);
-INSERT INTO OBJTYPE (OBJECT_TYPE_ID, PARENT_ID, CODE, NAME, DESCRIPTION) 
+INSERT INTO objtype (object_type_id, parent_id, code, name, description)
 VALUES (2, 1, 'Subtask', 'Подзадача', NULL);
-INSERT INTO OBJTYPE (OBJECT_TYPE_ID, PARENT_ID, CODE, NAME, DESCRIPTION) 
+INSERT INTO objtype (object_type_id, parent_id, code, name, description)
 VALUES (3, NULL, 'User', 'Пользователь', NULL);
-INSERT INTO OBJTYPE (OBJECT_TYPE_ID, PARENT_ID, CODE, NAME, DESCRIPTION) 
+INSERT INTO objtype (object_type_id, parent_id, code, name, description)
 VALUES (4, NULL, 'Category', 'Категория', NULL);
--- </OBJTYPE> ------------------------------------------------------------------
 
 
--- <ATTRTYPE> ------------------------------------------------------------------
--- Add Task attributes
-INSERT INTO ATTRTYPE (ATTR_ID, OBJECT_TYPE_ID, OBJECT_TYPE_ID_REF, CODE, NAME)  
-VALUES (1, 1, null, 'name', 'Название');
-INSERT INTO ATTRTYPE (ATTR_ID, OBJECT_TYPE_ID, OBJECT_TYPE_ID_REF, CODE, NAME)  
-VALUES (2, 1, null, 'date', 'Дата');
-INSERT INTO ATTRTYPE (ATTR_ID, OBJECT_TYPE_ID, OBJECT_TYPE_ID_REF, CODE, NAME)  
-VALUES (3, 1, null, 'priority', 'Приоритет');
-INSERT INTO ATTRTYPE (ATTR_ID, OBJECT_TYPE_ID, OBJECT_TYPE_ID_REF, CODE, NAME)  
-VALUES (4, 1, null, 'category', 'Категория');
-INSERT INTO ATTRTYPE (ATTR_ID, OBJECT_TYPE_ID, OBJECT_TYPE_ID_REF, CODE, NAME)  
-VALUES (5, 1, null, 'status', 'Статус');
+-- Add Task attrtype
+INSERT INTO attrtype (attr_id, object_type_id, object_type_id_ref, code, name)
+VALUES (1, 1, NULL, 'Date', 'Дата');
+INSERT INTO attrtype (attr_id, object_type_id, object_type_id_ref, code, name)
+VALUES (2, 1, NULL, 'Priority', 'Приоритет');
+INSERT INTO attrtype (attr_id, object_type_id, object_type_id_ref, code, name)
+VALUES (3, 1, NULL, 'Category', 'Категория');
+INSERT INTO attrtype (attr_id, object_type_id, object_type_id_ref, code, name)
+VALUES (4, 1, NULL, 'Status', 'Статус');
 
--- Add User attributes
-INSERT INTO ATTRTYPE (ATTR_ID,OBJECT_TYPE_ID,OBJECT_TYPE_ID_REF,CODE,NAME) 
-values(6, 1, NULL,'EMAIL','ПОЧТОВЫЙ АДРЕС');
-INSERT INTO ATTRTYPE (ATTR_ID,OBJECT_TYPE_ID,OBJECT_TYPE_ID_REF,CODE,NAME) 
-values(7, 1, NULL,'PASSWORD','ПАРОЛЬ');
-INSERT INTO ATTRTYPE (ATTR_ID,OBJECT_TYPE_ID,OBJECT_TYPE_ID_REF,CODE,NAME) 
-values(8, 1, NULL,'NAME','ИМЯ');
-INSERT INTO ATTRTYPE (ATTR_ID,OBJECT_TYPE_ID,OBJECT_TYPE_ID_REF,CODE,NAME) 
-values(9, 1, NULL,'SURNAME','ФАМИЛИЯ');
-INSERT INTO ATTRTYPE (ATTR_ID, OBJECT_TYPE_ID, OBJECT_TYPE_ID_REF, CODE, NAME)  
-VALUES (10, 3, 1, 'CREATE', 'Создает задачу');
+-- Add Subtask attrtype
+INSERT INTO attrtype (attr_id,object_type_id,object_type_id_ref,code,name)
+VALUES(5, 2, NULL,'Completed','Завершенный');
 
--- Add Category attrtype 
-INSERT INTO ATTRTYPE (ATTR_ID,OBJECT_TYPE_ID,OBJECT_TYPE_ID_REF,CODE,NAME)
-values(12, 4, NULL,'name','Имя');
-INSERT INTO ATTRTYPE (ATTR_ID,OBJECT_TYPE_ID,OBJECT_TYPE_ID_REF,CODE,NAME)
-values(13, 4, NULL,'position','Позиция');
+-- Add User attrtype
+INSERT INTO attrtype (attr_id,object_type_id,object_type_id_ref,code,name)
+VALUES(6, 3, NULL,'Email','Почтовый адрес');
+INSERT INTO attrtype (attr_id,object_type_id,object_type_id_ref,code,name)
+VALUES(7, 3, NULL,'Password','Пароль');
+INSERT INTO attrtype (attr_id,object_type_id,object_type_id_ref,code,name)
+VALUES(8, 3, NULL,'Surname','Фамилия');
 
--- </ATTRTYPE> ------------------------------------------------------------------
+-- Add Category attrtype
+INSERT INTO attrtype (attr_id,object_type_id,object_type_id_ref,code,name)
+VALUES(9, 4, NULL, 'Position', 'Позиция');
 
 
--- <OBJECTS> ------------------------------------------------------------------
--- Add new Task object 
-INSERT INTO OBJECTS (OBJECT_ID, PARENT_ID, OBJECT_TYPE_ID, NAME, DESCRIPTION) 
-VALUES (1, NULL, 1, 'Task1', NULL);
-
--- Add new User object 
-INSERT INTO OBJECTS (OBJECT_ID, PARENT_ID, OBJECT_TYPE_ID, NAME, DESCRIPTION) 
-VALUES (2, NULL, 3, 'User1', NULL);
--- </OBJECTS>------------------------------------------------------------------
-
--- <ATTRIBUTES> ------------------------------------------------------------------
--- Task Object - Fill attributes
-INSERT INTO ATTRIBUTES (ATTR_ID, OBJECT_ID, VALUE, DATE_VALUE)
-VALUES (1, 1, ?, NULL); /* name */
-INSERT INTO ATTRIBUTES (ATTR_ID, OBJECT_ID, VALUE, DATE_VALUE)
-VALUES (2, 1, NULL, ?); /* date */
-INSERT INTO ATTRIBUTES (ATTR_ID, OBJECT_ID, VALUE, DATE_VALUE)
-VALUES (3, 1, ?, NULL); /* priority */
-INSERT INTO ATTRIBUTES (ATTR_ID, OBJECT_ID, VALUE, DATE_VALUE)
-VALUES (4, 1, ?, NULL); /* category */
-INSERT INTO ATTRIBUTES (ATTR_ID, OBJECT_ID, VALUE, DATE_VALUE)
-VALUES (5, 1, ?, NULL); /* status */
-INSERT INTO ATTRIBUTES (ATTR_ID, OBJECT_ID, VALUE, DATE_VALUE)
-VALUES (6, 1, ?, NULL); /* subTaskList */
--- </ATTRIBUTES> ------------------------------------------------------------------
-
--- <OBJREFERENCE> ------------------------------------------------------------------
--- Óñòàíîâêà àññîöèàòèâíûõ ñâÿçåé
--- References between Attributes
-INSERT INTO OBJREFERENCE (ATTR_ID,  REFERENCE, OBJECT_ID)
-VALUES(10, 2, 1); /* (User1 = 2) -> (create = 11) -> (Task1 = 1) */
--- </OBJREFERENCE> ------------------------------------------------------------------
