b1febd8b9dc6be86549ae0002d53fe60cd3284ab
gadzzzz <bogdanhadzhiev@yahoo.com>
2015-12-14 21:45:49 +0300

    Error Fix + UserController

--- src/main/java/organizer/controllers/UserController.java
+++ src/main/java/organizer/controllers/UserController.java
@@ -1,6 +1,12 @@
 package organizer.controllers;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,10 +14,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import organizer.dao.api.TaskDao;
 import organizer.dao.api.UserDao;
+import organizer.logic.impl.MessageContent;
+import organizer.models.Task;
 import organizer.models.User;
 
 @Controller
@@ -21,10 +29,43 @@ public class UserController {
 	@Qualifier("userDaoImpl")
 	private UserDao userDaoImpl;
 	
-	@RequestMapping(value="/createuser",method=RequestMethod.GET)
-	public @ResponseBody String createUser(@RequestParam("email") String email,@RequestParam("password") String password,@RequestParam("name") String name,@RequestParam("surname") String surname){
+	@RequestMapping(value="/createuserform",method=RequestMethod.GET)
+	public String createUserForm(){
 		Locale.setDefault(Locale.ENGLISH);
-		User user = new User(email,password,name,surname);
-		return userDaoImpl.create(user);
+		return "createuser";
+	}
+	
+	@RequestMapping(value="/createuser",method=RequestMethod.POST)
+	public @ResponseBody String createUser(HttpServletRequest request){
+		String email =	request.getParameter("email");
+		String password =  request.getParameter("password");
+		
+		
+		//******Md5 Test********
+		MessageDigest messageDigest = null;
+		try {
+			messageDigest = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		messageDigest.update(password.getBytes(),0, password.length());  
+		String hashedPass = new BigInteger(1,messageDigest.digest()).toString(16);  
+		if (hashedPass.length() < 32) {
+		   hashedPass = "0" + hashedPass; 
+		}
+		//******Md5 Test********
+		
+		
+		String name =  request.getParameter("name");
+		String surname = request.getParameter("surname");
+		User user = new User(email,hashedPass,name,surname);
+		String message = MessageContent.ERROR;
+		try{
+			message = userDaoImpl.create(user);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return message;
 	}
 }
---
+++ src/main/webapp/views/createuser.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<html>
+<head></head>
+<body>
+<form action="createuser" method="post">
+<p>login:<input type="text" id="email" name="email"></p>
+<p>password:<input type="password" id="password" name="password" ></p>
+<p>name:<input type="text" id="name" name="name"></p>
+<p>surname:<input type="text" id="surname" name="surname"></p>
+<p><input type="submit" value="register"><input type="reset" value="reset"></p>
+</form>
+</body>
+</html>
