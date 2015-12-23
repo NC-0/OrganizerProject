c6673ba5ce7835a37e45040a62e78120a747a8ef
SergeyValevskyi <w0934014151@gmail.com>
2015-12-23 02:07:14 +0200

    Change place storage for category queries.

--- src/main/java/organizer/dao/api/CategoryDao.java
+++ src/main/java/organizer/dao/api/CategoryDao.java
@@ -8,14 +8,18 @@ import java.util.List;
 public interface CategoryDao {
 	void create(Category category);
 	void create(User user, Category category);
-	void delete();
+	void delete(int id);
 	void edit(Category category);
 	List<Category> get(int userId);
 
 	int OBJ_TYPE = 4;
+	int REF_ATTR = 9;
 
+	String SELECT_ID = UserDao.SELECT_ID;
 	String SELECT_USER_CATEGORIES = "SELECT cat_obj.NAME FROM objects cat_obj, OBJECTS usr_obj,objreference usr_cat_ref WHERE usr_cat_ref.ATTR_ID=9 AND cat_obj.OBJECT_ID=usr_cat_ref.REFERENCE AND usr_obj.OBJECT_ID=usr_cat_ref.OBJECT_ID AND usr_obj.OBJECT_ID=?";
 
+	String INSERT_CATEGORY_OBJECT = "INSERT INTO objects(parent_id,object_type_id,name,description) VALUES (NULL,4,?,NULL)";
+	String INSERT_REF_TO_USER = "INSERT INTO objreference (attr_id,object_id,reference) VALUES ("+REF_ATTR+",?,?)";
 
 	String DELETE = "DELETE FROM objects WHERE object_id = ? AND object_type_id = " + OBJ_TYPE;
 }
