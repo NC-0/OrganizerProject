78324d7d268ec00807f5fd0a0c39008d63521b76
SergeyValevskyi <w0934014151@gmail.com>
2015-12-23 02:11:29 +0200

    Added new place query in create method. Deleted old solution for create transaction.

--- src/main/java/organizer/dao/impl/CategoryDaoImpl.java
+++ src/main/java/organizer/dao/impl/CategoryDaoImpl.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import organizer.dao.api.CategoryDao;
-import organizer.dao.api.SubtaskDao;
 import organizer.logic.impl.SqlContent;
 import organizer.models.Category;
 import organizer.models.User;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -26,27 +22,10 @@ public class CategoryDaoImpl implements CategoryDao {
 		throw new UnsupportedOperationException("Need realization session and then we may get user in session.");
 	}
 
-	public void create(final User user, final Category category) {
-		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-			@Override
-			protected void doInTransactionWithoutResult(TransactionStatus status) {
-				try {
-					createCategory(user, category);
-				}
-				catch (Exception e) {
-					status.setRollbackOnly();
-				}
-			}
-		});
-	}
-
-	private void createCategory(User user, Category category) throws Exception {
-		Integer userId = jdbcTemplate.queryForObject(SqlContent.GET_OBJECT_ID_BY_EMAIL, new Object[]{user.getEmail()}, Integer.class);
-		if (userId != 0) {
-			Integer categoryId = jdbcTemplate.queryForObject(SqlContent.SELECT_NEXT_OBJECT_ID_VALUE, Integer.class);
-			jdbcTemplate.update(SqlContent.INSERT_CATEGORY_OBJECT, categoryId, category.getName());
-			jdbcTemplate.update(SqlContent.INSERT_USER_TO_CATEGORY_REFERENCE, userId, categoryId);
-		}
+	public void create(User user, Category category) {
+		jdbcTemplate.update(CategoryDao.INSERT_CATEGORY_OBJECT, category.getName());
+		Integer categoryId = jdbcTemplate.queryForObject(CategoryDao.SELECT_ID, Integer.class);
+		jdbcTemplate.update(CategoryDao.INSERT_REF_TO_USER, user.getId(), categoryId);
 	}
 
 	public void delete(int id) {
