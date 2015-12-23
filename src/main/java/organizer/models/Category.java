d9c1d333d410597ab77940d2db703381ed9529f5
Sergey Valevskyi <w093401451@gmail.com>
2015-12-23 02:13:06 +0200

    Added constructor with "name" parameter.

--- src/main/java/organizer/models/Category.java
+++ src/main/java/organizer/models/Category.java
@@ -4,6 +4,10 @@ public class Category {
 	private int id;
 	private String name;
 
+	public Category(String name) {
+		this.name = name;
+	}
+
 	public Category(int id, String name) {
 		this.id = id;
 		this.name = name;
