f589f155349d048db21722c0110767f64f177453
NataliaBezhenar <natalia.bezhenar@gmail.com>
2015-12-24 19:37:28 +0200

    Assign Category to the Task added to AttrType table

--- TableStructure_code.sql
+++ TableStructure_code.sql
@@ -96,6 +96,8 @@ INSERT INTO attrtype (attr_id,object_type_id,object_type_id_ref,code,name)
   VALUES (9, 3, 4,'Create Category','Создание категории');
 INSERT INTO attrtype (attr_id,object_type_id,object_type_id_ref,code,name)
   VALUES (10, 3, 1,'Create Task','Создание задачи');
+INSERT INTO attrtype (attr_id,object_type_id,object_type_id_ref,code,name)
+  VALUES (12, 1, 4,'Assign Category','Присвоение категории');  
 
 -- Objects trigger
 CREATE OR REPLACE TRIGGER object_id_generate_trigger
