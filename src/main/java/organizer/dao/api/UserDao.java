7c7ffb9cc3899b40d714cd8dd063c29af5b8f02c
Bogdan Hadzhiev <bogdanhadzhiev@yahoo.com>
2015-12-26 20:03:17 +0200

    Edit user dao

--- src/main/java/organizer/dao/api/UserDao.java
+++ src/main/java/organizer/dao/api/UserDao.java
@@ -4,10 +4,9 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import organizer.models.User;
 
-@Component
-@Scope("prototype")
 public interface UserDao {
 	boolean exist(String email);
+	boolean exist(int id);
 	String create(User user);
 	void delete(int id);
 	String edit(User user);
@@ -20,24 +19,24 @@ public interface UserDao {
 	int PASSWORD_ATTR = 7;
 	int SURNAME_ATTR  = 8;
 
-	String SELECT_COUNT = "SELECT COUNT(*) FROM attributes usr_attr WHERE usr_attr.attr_id = 6 AND usr_attr.value = ?";
-	String SELECT_ID    = "SELECT object_id.CURRVAL FROM dual";
+	String SELECT_COUNT_EMAIL  = "SELECT COUNT(*) FROM attributes usr_attr WHERE usr_attr.attr_id = " + EMAIL_ATTR + " AND usr_attr.value = ?";
+	String SELECT_COUNT_ID 	   = "SELECT COUNT(*) FROM objects usr_obj WHERE usr_obj.object_id = ? AND usr_obj.object_type_id=" + OBJ_TYPE;
+	String SELECT_ID       		= "SELECT object_id.CURRVAL FROM dual";
 
-	String INSERT          = "INSERT INTO objects (parent_id, object_type_id, name, description) VALUES (NULL, " + OBJ_TYPE + ", ?,NULL)";
-	String INSERT_EMAIL    = "INSERT INTO attributes (attr_id, object_id, value, date_value) VALUES (" + EMAIL_ATTR + ", ?, ?, NULL)";
-	String INSERT_ENABLED  = "INSERT INTO attributes (attr_id,object_id,value,date_value) VALUES (" + ENABLED_ATTR + ", ?, ?, NULL)";
-	String INSERT_PASSWORD = "INSERT INTO attributes (attr_id, object_id, value, date_value) VALUES (" + PASSWORD_ATTR + ", ?, ?, NULL)";
-	String INSERT_SURNAME  = "INSERT INTO attributes (attr_id, object_id, value, date_value) VALUES (" + SURNAME_ATTR + ", ?, ?, NULL)";
+	String INSERT          	= "INSERT INTO objects (parent_id, object_type_id, name, description) VALUES (NULL, " + OBJ_TYPE + ", ?,NULL)";
+	String INSERT_EMAIL    	= "INSERT INTO attributes (attr_id, object_id, value, date_value) VALUES (" + EMAIL_ATTR + ", ?, ?, NULL)";
+	String INSERT_ENABLED  	= "INSERT INTO attributes (attr_id,object_id,value,date_value) VALUES (" + ENABLED_ATTR + ", ?, ?, NULL)";
+	String INSERT_PASSWORD 	= "INSERT INTO attributes (attr_id, object_id, value, date_value) VALUES (" + PASSWORD_ATTR + ", ?, ?, NULL)";
+	String INSERT_SURNAME  	= "INSERT INTO attributes (attr_id, object_id, value, date_value) VALUES (" + SURNAME_ATTR + ", ?, ?, NULL)";
 
-	String DELETE_OBJECT = "DELETE objects WHERE object_id = ? AND object_type_id = " + OBJ_TYPE;
-	String DELETE_OBJECTS_REF_T0_USER = "DELETE objects WHERE object_id IN (SELECT reference FROM objreference WHERE object_id = ?)";
+	String UPDATE_NAME		= "UPDATE objects usr_obj SET usr_obj.name = ? WHERE usr_obj.object_id = ?";
+	String UPDATE_SURNAME 	= "UPDATE attributes usr_attr SET usr_attr.value = ? WHERE usr_attr.attr_id = " + SURNAME_ATTR + "  and usr_attr.object_id = ?";
+	String UPDATE_PASSWORD 	= "UPDATE attributes usr_attr SET usr_attr.value = ? WHERE usr_attr.attr_id = " + PASSWORD_ATTR + "  and usr_attr.object_id = ?";
 
-//	String SELECT_ID_BY_EMAIL = "SELECT object_ID FROM attributes WHERE attr_id = " + EMAIL_ATTR + " AND value = ?";
-//	String SELECT_NAME        = "SELECT name FROM objects WHERE object_id = ?";
-//	String SELECT_PASSWORD    = "SELECT value FROM attributes WHERE object_id = ? AND attr_id = " + PASSWORD_ATTR;
-//	String SELECT_SURNAME     = "SELECT value FROM attributes WHERE object_id = ? AND attr_id = " + SURNAME_ATTR;
-	
-	String GET_USER_INFO = "SELECT " +
+	String DELETE_OBJECT 					= "DELETE objects WHERE object_id = ? AND object_type_id = " + OBJ_TYPE;
+	String DELETE_OBJECTS_REF_T0_USER	= "DELETE objects WHERE object_id IN (SELECT reference FROM objreference WHERE object_id = ?)";
+
+	String SELECT_USER_BY_EMAIL = "SELECT " +
 		"obj.OBJECT_ID as userid, " +		// user id
 		"obj.NAME as username, " + 		// user name
 		"pass_attr.VALUE as password, " +	// password
@@ -55,6 +54,30 @@ public interface UserDao {
 		"surname_attr.attr_id = " + SURNAME_ATTR + " AND " +
 		"enable_attr.attr_id = " + ENABLED_ATTR + " AND " +
 		"email_attr.value = ? AND " +
+		"obj.object_id = email_attr.OBJECT_ID AND " +
+		"pass_attr.OBJECT_ID = email_attr.OBJECT_ID AND " +
+		"surname_attr.object_id = email_attr.OBJECT_ID AND " +
+		"enable_attr.object_id = email_attr.OBJECT_ID";
+
+	String SELECT_USER_BY_ID = "SELECT " +
+		"obj.OBJECT_ID as userid, "+
+		"obj.NAME as username, " +
+		"pass_attr.VALUE as password, " +
+		"email_attr.VALUE as email, " +
+		"surname_attr.VALUE as surname, " +
+		"enable_attr.VALUE as enabled " +
+		"FROM " +
+		"attributes email_attr, " +
+		"attributes pass_attr, " +
+		"attributes surname_attr, " +
+		"attributes enable_attr, " +
+		"objects obj " +
+		"WHERE " +
+		"email_attr.attr_id = 6 AND " +
+		"pass_attr.attr_id = 7 AND " +
+		"surname_attr.attr_id = 8 AND " +
+		"enable_attr.attr_id = 11 AND " +
+		"obj.object_id = ? AND " +
 		"obj.object_id = email_attr.OBJECT_ID AND " +
 		"pass_attr.OBJECT_ID = email_attr.OBJECT_ID AND " +
 		"surname_attr.object_id = email_attr.OBJECT_ID AND " +
--- src/main/java/organizer/dao/impl/UserDaoImpl.java
+++ src/main/java/organizer/dao/impl/UserDaoImpl.java
@@ -2,68 +2,109 @@ package organizer.dao.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import organizer.dao.api.UserDao;
+import organizer.dao.mappers.UserRowMapper;
 import organizer.logic.impl.MessageContent;
 import organizer.models.User;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
+@Component
+@Scope("prototype")
 public class UserDaoImpl implements UserDao {
 	@Autowired
 	@Qualifier("jdbcTemplate")
 	private JdbcTemplate jdbcTemplate;
 
 	public boolean exist(String email) {
-		boolean exist = jdbcTemplate.queryForObject(UserDao.SELECT_COUNT, new Object[]{email}, Integer.class) != 0;
+		boolean exist = jdbcTemplate.queryForObject(
+			SELECT_COUNT_EMAIL,
+			new Object[]{email},
+			Integer.class) != 0;
 		return exist;
 	}
-	
+
+	public boolean exist(int id) {
+		boolean exist = jdbcTemplate.queryForObject(
+			SELECT_COUNT_ID,
+			new Object[]{id},
+			Integer.class) != 0;
+		return exist;
+	}
+
 	public String create(User user) {
 		if (!exist(user.getEmail())) {
-			jdbcTemplate.update(UserDao.INSERT, user.getName());
-			Integer objectsCurrentValue = jdbcTemplate.queryForObject(UserDao.SELECT_ID, Integer.class);
-			jdbcTemplate.update(UserDao.INSERT_EMAIL, objectsCurrentValue, user.getEmail());
-			jdbcTemplate.update(UserDao.INSERT_PASSWORD, objectsCurrentValue, user.getPassword());
-			jdbcTemplate.update(UserDao.INSERT_SURNAME, objectsCurrentValue, user.getSurname());
-			jdbcTemplate.update(UserDao.INSERT_ENABLED, objectsCurrentValue, 1);//1-enabled
+			jdbcTemplate.update(
+				INSERT,
+				user.getName());
+			Integer objectsCurrentValue = jdbcTemplate.queryForObject(
+				SELECT_ID, Integer.class);
+			jdbcTemplate.update(
+				INSERT_EMAIL,
+				objectsCurrentValue,
+				user.getEmail());
+			jdbcTemplate.update(
+				INSERT_PASSWORD,
+				objectsCurrentValue,
+				user.getPassword());
+			jdbcTemplate.update(
+				INSERT_SURNAME,
+				objectsCurrentValue,
+				user.getSurname());
+			jdbcTemplate.update(
+				INSERT_ENABLED,
+				objectsCurrentValue,
+				1);//1-enabled
 			return String.format(MessageContent.USER_CREATED, user.getEmail());
 		}
 		return String.format(MessageContent.USER_ALREADY_EXIST, user.getEmail());
 	}
 
 	public void delete(int id) {
-		jdbcTemplate.update(DELETE_OBJECTS_REF_T0_USER, id);
-		jdbcTemplate.update(DELETE_OBJECT, id);
+		jdbcTemplate.update(
+			DELETE_OBJECTS_REF_T0_USER,
+			id);
+		jdbcTemplate.update(
+			DELETE_OBJECT,
+			id);
 	}
 
 	public String edit(User user) {
-		// TODO Auto-generated method stub
+		jdbcTemplate.update(
+			UPDATE_NAME,
+			user.getName(),
+			user.getId());
+		jdbcTemplate.update(
+			UPDATE_SURNAME,
+			user.getSurname(),
+			user.getId());
+		jdbcTemplate.update(
+			UPDATE_PASSWORD,
+			user.getPassword(),
+			user.getId());
 		return null;
 	}
 
 	public User get(String email) {
-		// check if user exists 
 		if (exist(email)) {
-			User user = jdbcTemplate.queryForObject(UserDao.GET_USER_INFO, new Object[]{email}, new RowMapper<User>() {
-				public User mapRow(ResultSet resultSet, int i) throws SQLException {
-					User user = new User(resultSet.getString("email"),resultSet.getString("password"),resultSet.getString("username"),resultSet.getString("surname"));
-					user.setRole("USER_ROLE");
-					user.setId(Integer.valueOf(resultSet.getString("userid")));
-					user.setEnabled(Boolean.valueOf(resultSet.getString("enabled")));
-					return user;
-				}
-			});
+			User user = jdbcTemplate.queryForObject(
+				SELECT_USER_BY_EMAIL,
+				new Object[]{email},
+				new UserRowMapper());
 			return user;
 		}
-		// if user not exists
 		return null;
 	}
 
-	// TODO: Implement get user by id method
-	@Override
 	public User get(int id) {
-		throw new UnsupportedOperationException();
+		if (exist(id)) {
+			User user = jdbcTemplate.queryForObject(
+				SELECT_USER_BY_ID,
+				new Object[]{id},
+				new UserRowMapper());
+			return user;
+		}
+		return null;
 	}
 }
