7b9e2233fddbee6a8789a3c69558c99dd4e711b2
Sergey Valevskyi <w093401451@gmail.com>
2016-01-10 01:35:30 +0200

    Added constructor and assignment User. CategoryRowMapper

--- src/main/java/organizer/dao/cache/CategoryRowMapper.java
+++ src/main/java/organizer/dao/cache/CategoryRowMapper.java
@@ -7,11 +7,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CategoryRowMapper extends CachedRowMapper<Category> {
+	private User user;
+
+	public CategoryRowMapper(User user) {
+		this.user = user;
+	}
+
+	public CategoryRowMapper(){
+	this(null);
+	}
+
 	@Override
 	public Category createObject(int id, ResultSet resultSet) throws SQLException {
 		Category category = new Category(
 			resultSet.getString("name")
 		);
+		category.setUser(user);
 		category.setId(Integer.valueOf(resultSet.getString("id")));
 		return category;
 	}
