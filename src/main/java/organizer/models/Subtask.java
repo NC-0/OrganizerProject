5e744f1743e831351fbcffb7d66f7d87a4dd83e7
Sergey Valevskyi <w093401451@gmail.com>
2016-01-09 22:32:40 +0200

    Added equals and hashCode for Subtask.

--- src/main/java/organizer/models/Subtask.java
+++ src/main/java/organizer/models/Subtask.java
@@ -44,4 +44,38 @@ public class Subtask {
 	public void setTask(Task task) {
 		this.task = task;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj instanceof Subtask) {
+			Subtask subtask = (Subtask) obj;
+			return
+					(
+							equals(id, subtask.getId()) &&
+									equals(name, subtask.getName()) &&
+									equals(completed, subtask.isCompleted())
+					);
+		}
+		return false;
+
+	}
+
+	private boolean equals(Object firstObject, Object secondObject) {
+		return (firstObject == secondObject || (firstObject != null && firstObject.equals(secondObject)));
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 37;
+		hash = hash * 17 + hashCode(id);
+		hash = hash * 17 + hashCode(name);
+		hash = hash * 17 + hashCode(completed);
+		return hash;
+	}
+
+	private int hashCode(Object object){
+		return object !=null? object.hashCode():0;
+	}
 }
