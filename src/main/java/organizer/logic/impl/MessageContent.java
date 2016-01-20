6b3b9d1675288b0c6e530a5193f6eb2bbc00e338
Bogdan Hadzhiev <bogdanhadzhiev@yahoo.com>
2016-01-20 20:38:12 +0200

    Update messages

--- src/main/java/organizer/logic/impl/MessageContent.java
+++ src/main/java/organizer/logic/impl/MessageContent.java
@@ -5,7 +5,9 @@ public class MessageContent {
 	
 	public final static String USER_CREATED = "New user %s created";
 	
-	public final static String USER_ALREADY_EXIST = "User with %s email already exist";
+	public final static String USER_ALREADY_EXIST = "User with '%s' email already exist";
+
+	public final static String USER_NOT_EXIST = "User with '%s' email not exist";
 	
 	public final static String ERROR = "Something bad happens, sorry";
 
@@ -29,11 +31,15 @@ public class MessageContent {
 
 	public final static String MAIL_TEXT_VERIFICATION = "<html><body><b>Hello, %s! You succesfully registered on SuperOrganizer.com. Please go to this link <a href='http://localhost:8081/verification?verific=%s'>http://localhost:8081/verification?verific=%s</a></body></html>";
 
+	public final static String MAIL_TEXT_FORGET = "<html><body><b>Hello, %s! You forgot password on SuperOrganizer.com. You new password: %s</body></html>";
+
 	public final static String VERIFY_ERROR = "Unrecognize verification id";
 
 	public final static String VERIFY_SUCCESSFULL = "Verification successfull";
 
-	public final static String EMAIL_TASK_NOTIFY = "<html><body><b>Hello, %s! You tasks on tomorrow:<br><ul>%s</ul></body></html>";
+	public final static String EMAIL_TASK_NOTIFY = "<html><body><b>Hello, %s!<br> You tasks on tomorrow:<br><table>%s</table></body></html>";
 
-	public final static String EMAIL_TASK = "<li>%s</li>";
+	public final static String EMAIL_TASK = "<tr><td bgcolor='%s'>%s</td></tr>";
+
+	public final static String EMAIL_PASS = "New password send to you email";
 }
