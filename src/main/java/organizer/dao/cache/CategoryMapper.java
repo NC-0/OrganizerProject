aa0e3063b980fdf0b6ebf668a0d2ac3927e6c382
Bogdan Hadzhiev <bogdanhadzhiev@yahoo.com>
2016-02-01 13:47:31 +0200

    Cache test

--- src/main/java/organizer/dao/cache/CategoryMapper.java
+++ src/main/java/organizer/dao/cache/CategoryMapper.java
@@ -1,26 +1,3 @@
-//package organizer.dao.cache;
-//
-//import organizer.models.Category;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//
-//public class CategoryMapper extends CachedMapper<Category> {
-//
-//	public CategoryMapper(CacheImpl cache) {
-//		setCacheImpl(cache);
-//	}
-//
-//	@Override
-//	public Category createObject(int id, ResultSet resultSet) throws SQLException {
-//		Category category = new Category(
-//			resultSet.getString("name")
-//		);
-//		category.setId(id);
-//		return category;
-//	}
-//}
-
 package organizer.dao.cache;
 
 import organizer.models.Category;
